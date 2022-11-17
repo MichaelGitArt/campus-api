6d569e2c4372945a47a9609777fab4adfcdb770f
Michael Kryvoruchko <mk01@rpsdev.ch>
2022-11-17 09:43:55 +0200

    finish

--- main.mjs
+++ main.mjs
@@ -1,9 +1,22 @@
 import fastify from 'fastify'
 import ping from 'ping'
+import cors from '@fastify/cors'
 
 const app = fastify()
 
-app.get('/', async(request, reply) => {
+app.register(cors, {
+  origin: ['http://localhost:3000', 'https://campus-prostir.netlify.app'],
+  methods: ['GET'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Access-Control-Allow-Credentials'],
+})
+
+app.get('/', (request, reply) => {
+  return {
+    data: 'Hello World!',
+  }
+})
+
+app.get('/check-electricity', async(request, reply) => {
   const address = '176.37.90.242'
   const pingResult = await ping.promise.probe(address)
 
@@ -12,7 +25,7 @@ app.get('/', async(request, reply) => {
   }
 })
 
-console.log('http://localhost:3001')
+console.log('http://localhost:3001/check-electricity')
 app.listen({
   port: 3001,
 })
